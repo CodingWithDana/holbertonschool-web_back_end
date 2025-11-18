c13046d2f31a927e7a93aeff68d2792e654daef7
Dana Le <danadaole@gmail.com>
2025-11-18 11:54:42 +1100

    basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+Module that provides a simple sum function
+"""
+def add(a: float, b: float) -> float:
+    """ Add two floating-point numbers and return their sum """
+    return a + b
