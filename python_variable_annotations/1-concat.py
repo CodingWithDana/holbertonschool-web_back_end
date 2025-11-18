75db0720f5da13d1d21feca656190f6fc398d232
Dana Le <danadaole@gmail.com>
2025-11-18 12:01:34 +1100

    basic annotations - concatenate

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Module that takes two strings and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Concatenate two strings """
+    return str1 + str2
