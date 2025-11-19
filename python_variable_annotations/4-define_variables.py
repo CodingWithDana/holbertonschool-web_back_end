5b4dfdf19f9a5ee9f289d6718099f6887fec2b6a
Dana Le <danadaole@gmail.com>
2025-11-19 11:44:11 +1100

    define and annotate variables with assignment

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" Module that defines and annotates the variables with the specified values """
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
