6e77b22fa5ebfac1b1d167737c4667a02635e712
Dana Le <danadaole@gmail.com>
2025-11-19 17:30:07 +1100

    complex types - list of integers and floats

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that provides a function which takes a list of integers and floats
+and returns the sum as a float
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]):
+    """ Calc the sum of a list of integers and floats """
+    return (mxd_lst)
