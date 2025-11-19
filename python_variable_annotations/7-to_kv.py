77e3af387bd66a283b4ed3e729b0a3623ed29606
Dana Le <danadaole@gmail.com>
2025-11-19 17:41:17 +1100

    complex types - from string and int/float to tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -10,6 +10,7 @@ and should be annotated as a float
 """
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ Return a tuple with a string and the square of a number as a float """
     return (k, (v ** 2))
