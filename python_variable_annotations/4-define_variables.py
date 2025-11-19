664cabe4fce793c4455eb12dbb0e2c1ac982d31f
Dana Le <danadaole@gmail.com>
2025-11-19 17:08:56 +1100

    define and annotates different variable types

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-""" Module that defines and annotates the variables with the specified values """
+"""
+Module that defines and annotates the variables 
+with the specified values
+"""
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
