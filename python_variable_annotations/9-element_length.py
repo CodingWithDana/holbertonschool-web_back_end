174a6b303837c28df57dc7f9c864a0737f23c2c2
Dana Le <danadaole@gmail.com>
2025-11-19 17:53:30 +1100

    complex types - duck type an iterable obj

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Module that provide a function to return values with the appropriate types
+"""
+from typing import List, Tuple, Iterable, Sequence
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
