a69f8b7224c60e5f43ca6e1008edcc5afc62ea23
Dana Le <danadaole@gmail.com>
2025-11-30 01:24:10 +1100

    write a script that provides a coroutine that generates 10 random numbers

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -13,11 +13,11 @@ from typing import Generator
 async def async_generator() -> Generator[float, None, None]:
     """
     Generate asynchronously 10 random numbers betwen 0 and 10
-    
+
     This coroutine loops 10 times. On each iteration, it asynchronously waits
     for 1 second, generates a random number between 0 and 10
     """
-    for _  in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         random_number = uniform(0, 10)
         yield random_number
