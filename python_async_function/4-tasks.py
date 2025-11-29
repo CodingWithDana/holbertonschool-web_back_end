bc0a0fed003dc923e80f93b4cfdb60563b520282
Dana Le <danadaole@gmail.com>
2025-11-30 00:55:01 +1100

    write a script that provides a function to run multiple asynchronous tasks using task_wait_random

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Module that provides a function to run multiple asynchronous tasks using task_wait_random
+Module that provides a function to run multiple asynchronous tasks
+using task_wait_random
 """
 
 
@@ -12,8 +13,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Run multiple asynchronous tasks that wait for a random delay and return results
-    in the order of completion
+    Run multiple asynchronous tasks that wait for a random delay and
+    return results in the order of completion
     """
     coroutines = [task_wait_random(max_delay) for _ in range(n)]
     result = []
