b3d892707cc38f73a713742bcdba905b15830234
Dana Le <danadaole@gmail.com>
2025-11-29 18:31:57 +1100

    write a script that provides an async function to wait for a random delay and return it

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module that provides a async function to wait for a random delay and return it
+"""
+import asyncio
+import random 
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Asynchronous coroutine that waits for a random delay
+    
+    uniform() method: returns a random floating number between
+    the two specified numbers (both included)
+    """
+    # random.uniform(start_of_range, end_of_range) to create a float delay
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay) # for asynchronuous waiting
+    return delay
