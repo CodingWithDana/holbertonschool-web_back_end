94dd2252db4b58dcb800d50d987d9b1a09fa05f7
Dana Le <danadaole@gmail.com>
2026-01-14 17:56:45 +1100

    using Process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+    const yourName = process.stdin.read();
+    if (yourName !== null) {
+        process.stdout.write('Your name is: ${yourName}');
+    }
+});
+
+process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+});
