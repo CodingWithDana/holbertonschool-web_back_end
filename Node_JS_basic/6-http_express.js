049cd0795397cbbf5b7d3c8278e7185dfcef0928
Dana Le <danadaole@gmail.com>
2026-01-15 17:16:46 +1100

    create a small HTTP server using Express

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+// express() creates the server
+const app = express();
+
+// define the route for '/'
+app.get('/', (req, res) => {
+  //  send 'Hello' as the body
+  res.send('Hello!');
+});
+
+// start the server
+app.listen(1245);
+
+module.exports = app;
